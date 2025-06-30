0fb42d493d9d27116d297bb7f22983fc1899ed53
Julia Pereira Silva <jupsilva@sbt.com.br>
2025-06-30 16:58:44 -0300

    project-study

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { getUsers } from "@/services/users";
 import { useEffect, useState } from "react";
 import './style.css'
 
+
 export default function Home() {
 
   const [users, setUsers] = useState<IUser[]>([])
@@ -42,52 +43,113 @@ export default function Home() {
   }, [])
 
 
+  // Card Info User
+  const [cardVisible, setCardVisible] = useState(false)
+
+  const handleClickMore = () => {
+    setCardVisible(!cardVisible)
+  }
+
+
+  // Implementando Busca
+  const [search, setSearch] = useState('')
+
+  const userFilter = users.filter((user) =>
+    user.firstName.toLowerCase().includes(search.toLowerCase())
+  );
+
+  // Se os usuarios não forem encontrados
+  if (userFilter.length === 0) {
+    console.log("Usuário não encontrado");
+  } else {
+    console.log("Usuários encontrados:", userFilter);
+  }
+
+
+
+
   return (
     <div className="flex flex-col justify-center items-center gap-10">
-      <h1 className="text-2xl">Usuários</h1>
+
+      <div className="bg-white w-full text-black flex justify-between p-2">
+        <h1 className="text-2xl">Usuários</h1>
+
+        <div className="input-group">
+
+          <div className="input-icon">
+            <img
+              className="w-6 h-6"
+              src="./img/search.png" alt="" />
+          </div>
+
+          <input
+            value={search}
+            // Implementação de evento quando for alterado o valor
+            onChange={(ev) => setSearch(ev.target.value)}
+            id="search"
+            placeholder="Search user"
+            type="text" />
+        </div>
+
+      </div>
+
 
       <div className="grid grid-cols-2 gap-10">
-        {users?.map((user) => (
-          <div
-            className="bg-white h-45 w-100 flex flex-col  text-black rounded-lg p-4"
-            key={user.id}>
 
+        {userFilter.length === 0 ? (
+          <p>User not found</p>
+        ) :
+          userFilter?.map((user) => (
             <div
-              id="container-img"
-              className="flex justify-start gap-2">
+              className="bg-white h-45 w-100 flex flex-col  text-black rounded-lg justify-center items-center"
+              key={user.id}>
 
-              <img
-                className=""
-                src={user.image} alt="" />
-              <div className="flex flex-col gap-2">
+              <div
+                id="container"
+                className="flex justify-start gap-2">
 
-                <div className="flex gap-2 text-2xl">
-                  <h1>{user.firstName}</h1>
-                  <h1>{user.lastName}</h1>
+                <div className="container-img">
+                  <img
+                    src={user.image} alt="" />
                 </div>
 
-                <h1>{user.phone}</h1>
-                <h1 className="text-sm">{user.email}</h1>
+                <div className="flex flex-col gap-2">
 
-                <button
-                id="buttonCard" 
-                className="bg-gray-300 rounded-2xl text-white w-[50%] text-sm">
-                  Exibir Perfil
-                </button>
+                  <div className="flex gap-2 text-2xl">
+                    <h1>{user.firstName}</h1>
+                    <h1>{user.lastName}</h1>
+                  </div>
+
+                  <h1>{user.phone}</h1>
+                  <h1 className="text-sm">{user.email}</h1>
+
+                  <button
+                    onClick={handleClickMore}
+                    id="buttonCard"
+                    className="bg-gray-300 rounded-2xl text-white w-[50%] text-sm">
+                    {cardVisible ? 'Fechar' : 'Exibir Perfil'}
+
+                  </button>
+
+                  {cardVisible && (
+                    <div
+                      id="cardInfo">
+                      <img
+                        className="h-5 w-5"
+                        src="./img/location.png" alt="" />
+                      <p>oi</p>
+                    </div>
+                  )}
 
 
-                <div id="cardInfo">
-                  <p>oi</p>
+
                 </div>
 
-                
               </div>
 
+
             </div>
-
-
-          </div>
-        ))}
+          ))}
 
 
       </div>
@@ -102,5 +164,6 @@ export default function Home() {
 
 
     </div>
+
   );
 }
--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,26 @@
-let button = document.getElementById('buttonCard')
-let card = document.getElementById('cardInfo')
+// let button = document.getElementById('buttonCard')
+// let card = document.getElementById('cardInfo')
 
-button.addEventListener('click', function() {
-    card.classList.toggle('elevated')
-})
+// button.addEventListener('click', function() {
+//     card.classList.toggle('elevated')
+// })
+
+
+
+// Pesquisa de usuarios de acordo com o nome 
+const searchUser = document.getElementById('search')
+
+searchUser.addEventListener('input', (event) => {
+    const value = (event.target.value)
+
+    // Colocando nossa função 
+    console.log(formatString(value))
+} )
+
+// Formatando
+function formatString(value){
+    return value
+        .toLoweCase()
+        // Tirar os espaços 
+        .trim();
+}
